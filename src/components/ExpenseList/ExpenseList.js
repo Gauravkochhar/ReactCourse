0b8ba2216948bd374853a840a92aecded857c4fb
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2022-03-07 09:56:32 +0530

    Delete Expense with expense id

--- src/components/ExpenseList/ExpenseList.js
+++ src/components/ExpenseList/ExpenseList.js
@@ -4,12 +4,18 @@ const ExpenseList = (props) => {
 
     let expenseListContent = 'No Expense found';
 
+    function onDeleteExpenseItem(expenseId) {
+        props.onDeleteExpense(expenseId);
+    }
+
     function getExpenseList() {
         if(!props.expenseList.length) {
             return <p>{expenseListContent}</p>
         } else {
             return props.expenseList.map((expense) => {
-                return <Expense key={expense.id} id={expense.id} name={expense.name} amount={expense.amount}></Expense>
+                return <div><Expense key={expense.id} id={expense.id} name={expense.name} amount={expense.amount}></Expense>
+                    <button onClick={() => onDeleteExpenseItem(expense.id)}>Delete</button>
+                </div>
             })
         }
     }
