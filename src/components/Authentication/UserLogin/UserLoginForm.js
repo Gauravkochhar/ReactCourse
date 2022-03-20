3d7adc9174a7484a6820b40d356ab4eb57f7beb0
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2022-03-20 10:48:06 +0530

    12th Day: Reducer Completed

--- src/components/Authentication/UserLogin/UserLoginForm.js
+++ src/components/Authentication/UserLogin/UserLoginForm.js
@@ -1,39 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+
+
 
 export const UserLoginForm = (props) => {
-    const [email, setEmail] = useState('');
-    const [ isEmailValid, setIsEmailValid] = useState(false);
-    const [password, setPassword] = useState('');
-    const [isPassValid, setIsPassValid] = useState(false);
-    const [formValid, setFormValid] = useState(false);
+    const loginFormReducer = (oldState, action) => {
+        if(action.type == 'EMAIL_CHANGE') {
+            return {
+                ...oldState,
+                email: {
+                    value: action.payload.value,
+                    isValid: action.payload.isValid
+                }
+            }
+        }
+        if(action.type == 'PASS_CHANGE') {
+            return {
+                ...oldState,
+                password: {
+                    value: action.payload.value,
+                    isValid: action.payload.isValid
+                }
+            }
+        }
+        // return { email: {value: '', isValid: ''}, password: {value: '', isValid: ''}};
+    }
 
-    useEffect(() => {
-        console.log('reevaluate useeffect code')
-        const emailValidate = email.includes('@') && (email.length > 6);
-        const passValidate = (password.length > 6);
-        setIsEmailValid(() => {
-            return emailValidate;
-        });
-        setIsPassValid(() => {
-            return passValidate;
-        });
-        setFormValid(() => {
-            return emailValidate && passValidate;
-        });
-    }, [email, password])
+    const initialState = { email: {value: '', isValid: ''}, password: {value: '', isValid: ''}};
+    const [ state, dispatchFn] = useReducer(loginFormReducer, initialState);
+    
+    // const [email, setEmail] = useState('');
+    // const [ isEmailValid, setIsEmailValid] = useState(false);
+    // const [password, setPassword] = useState('');
+    // const [isPassValid, setIsPassValid] = useState(false);
+    // const [formValid, setFormValid] = useState(false);
+    // useEffect(() => {
+    //     console.log('reevaluate useeffect code')
+    //     const emailValidate = email.includes('@') && (email.length > 6);
+    //     const passValidate = (password.length > 6);
+    //     setIsEmailValid(() => {
+    //         return emailValidate;
+    //     });
+    //     setIsPassValid(() => {
+    //         return passValidate;
+    //     });
+    //     setFormValid(() => {
+    //         return emailValidate && passValidate;
+    //     });
+    // }, [email, password])
     
     function emailChangeHandler(e) {
-        setEmail(e.target.value);
+        dispatchFn({ type: 'EMAIL_CHANGE', payload: {
+            value: e.target.value,
+            isValid: e.target.value.includes('@') && (e.target.value.length > 6)
+        }});
     }
 
     function passwordChangeHandler(e) {
-        setPassword(e.target.value);
+        dispatchFn({ type: 'PASS_CHANGE', payload: {
+            value: e.target.value,
+            isValid: e.target.value.length > 6
+        }});
     }
 
     function onSubmitLoginForm(event) {
         event.preventDefault();
-        const isEmailValid = email;
-        const isPassValid = password;
+        const isEmailValid = state.email.value;
+        const isPassValid = state.password.value;
+        console.log(state.email.value);
+        console.log(state.password.value);
         if(isEmailValid && isPassValid) {
             localStorage.setItem('isUserLoggedIn', 'true');
             props.updateLoginState(true);
@@ -47,14 +81,14 @@ export const UserLoginForm = (props) => {
         <h3>User Login Form</h3>
         <form onSubmit={onSubmitLoginForm}>
         <div className='form-control'>
-            <input type="text" placeholder='Registered Email' name="email"value={email}
+            <input type="text" placeholder='Registered Email' name="email" value={state.email.value}
             onChange={(e) => emailChangeHandler(e)}></input>
             </div>
             <div className='form-control'>
-            <input type="password" placeholder='Passsword' name="password" value={password}
+            <input type="password" placeholder='Passsword' name="password" value={state.password.value}
             onChange={(e) => passwordChangeHandler(e)}></input>
             </div>
-            <button type="submit" disabled={!formValid}>Login</button>
+            <button type="submit" disabled={!(state.email.isValid && state.password.isValid)}>Login</button>
         </form>
     </React.Fragment>
 }
