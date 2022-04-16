a83ecbb16a29c19b4c2f18ed1b8ceeefdc816bfa
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2022-04-16 10:23:37 +0530

    Day: 18th: 1- State define in CBC 2- How to use useEffect functionality in CBC 3- How to use useContext functionality in CBC

--- src/components/ClassBasedComponents/Demo/Demo.js
+++ src/components/ClassBasedComponents/Demo/Demo.js
@@ -1,8 +1,43 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import AuthContext from "../../../store/AuthContext/AuthContext";
 
 class Demo extends Component {
+
+    static contextType = AuthContext;
+    
+    constructor() {
+        super();
+        this.state = {
+            showMessage: true
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate(prevState, prevProps) {
+        if(prevState.showMessage !=  this.state.showMessage) {
+            // this.setState()
+        }
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    toggleHandler() {
+        this.setState((oldState) => {
+            return {showMessage: !oldState.showMessage}
+        })
+    }
+
     render() {
-        return <p>Hello {this.props.name}</p>
+        const dsfd = 1;
+        return <React.Fragment>
+            { this.state.showMessage ? <p>Hello {this.props.name}</p>: ''}
+            <button onClick={this.toggleHandler.bind(this)}>Toggle</button>
+        </React.Fragment> 
     }
 }
 
